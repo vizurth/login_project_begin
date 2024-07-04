d62881749efafad6452f9849c3377db5add703da
vizurth <timrsamara@gmail.com>
2024-07-04 21:16:18 +0400

    that all

--- login_project_begin/main.cpp
+++ login_project_begin/main.cpp
@@ -1,14 +1,51 @@
-//
-//  main.cpp
-//  login_project_begin
-//
-//  Created by Тимур Адиатуллин on 04.07.2024.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool checkData(string un, string pw){
+    string usernameIN, passwordIN;
+    ifstream file(un + ".txt");
+    if (file.is_open()){
+        getline(file, usernameIN);
+        getline(file, passwordIN);
+        file.close();
+        if (un == usernameIN && pw == passwordIN){
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+void includeData(string un, string pw){
+    ofstream file(un + ".txt", ios_base::out);
+    file << un << endl;
+    file << pw << endl;
+}
+
+int main(){
+    int choose;
+    cout << "1. Register" << endl;
+    cout << "2. Login" << endl;
+    cout << "Choose what you wanna do: ";
+    cin >> choose;
+    string username, password;
+    cout << "Write the Username: ";
+    cin >> username;
+    cout << "Write the Password: ";
+    cin >> password;
+    if (choose == 1){
+        includeData(username, password);
+    } else if (choose == 2){
+        if (checkData(username, password)){
+            cout << "YOU IN" << endl;
+        } else {
+            cout << "YOUR PASSWORD WRONG" << endl;
+        }
+    }
+    
     return 0;
 }
