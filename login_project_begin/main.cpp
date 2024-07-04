42616fc1bd2c70d6f06f7f433667a2e37b8aa463
vizurth <timrsamara@gmail.com>
2024-07-04 21:14:10 +0400

    Initial Commit

---
+++ login_project_begin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  login_project_begin
+//
+//  Created by Тимур Адиатуллин on 04.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
